aa470e8aa72dc5bea60b6071875a51e37db30f37
Ivar Johansson <ivar@webbmaffian.se>
2024-02-08 18:47:40 +0100

    Switched from sync.Cond to channel

--- bus.go
+++ bus.go
@@ -26,13 +26,9 @@ func NewBusWithWorkers(capacity int, workers int) *Bus {
 // is filled up without any worker, there will be deadlock. This should only be used in rare cases.
 func NewBusWithoutWorker(capacity int) *Bus {
 	b := &Bus{
-		queue:   make([]event, capacity),
-		subs:    make(map[subscription][]func(unsafe.Pointer)),
-		maxSize: capacity,
+		queue: make(chan event, capacity),
+		subs:  make(map[subscription][]func(unsafe.Pointer)),
 	}
-
-	b.pubCond = sync.Cond{L: &b.mu}
-	b.workCond = sync.Cond{L: &b.mu}
 
 	return b
 }
@@ -60,69 +56,30 @@ type subscription struct {
 type SubFn[T any] func(v *T)
 
 type Bus struct {
-	queue    []event
-	subs     map[subscription][]func(unsafe.Pointer)
-	start    int
-	size     int
-	maxSize  int
-	mu       sync.Mutex
-	pubCond  sync.Cond
-	workCond sync.Cond
-	closing  bool
-	closed   bool
+	queue chan event
+	subs  map[subscription][]func(unsafe.Pointer)
+	mu    sync.RWMutex
+	wg    sync.WaitGroup
 }
 
 func (b *Bus) Size() int {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	return b.size
+	return len(b.queue)
 }
 
 func (b *Bus) MaxSize() int {
-	return b.maxSize
+	return cap(b.queue)
 }
 
 func (b *Bus) Empty() bool {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	return b.empty()
-}
-
-//go:inline
-func (b *Bus) empty() bool {
-	return b.size == 0
+	return len(b.queue) == 0
 }
 
 func (b *Bus) Full() bool {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	return b.full()
+	return len(b.queue) == cap(b.queue)
 }
 
-//go:inline
-func (b *Bus) full() bool {
-	return b.size == b.maxSize
-}
-
-func (b *Bus) Close(graceful ...bool) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	b.closing = true
-
-	if len(graceful) > 0 && graceful[0] {
-		for !b.empty() {
-			b.pubCond.Wait()
-		}
-	}
-
-	b.closed = true
-
-	b.workCond.Broadcast()
-	b.pubCond.Broadcast()
+func (b *Bus) Close() {
+	close(b.queue)
 }
 
 func (b *Bus) _Sub(topic Topic, typ unsafe.Pointer, cb unsafe.Pointer) {
@@ -139,73 +96,24 @@ func (b *Bus) _Sub(topic Topic, typ unsafe.Pointer, cb unsafe.Pointer) {
 // Spawns a new worker. This function is blocking, and should be run in a goroutine.
 func (b *Bus) Worker() {
 	for {
-		b.mu.Lock()
+		ev, ok := <-b.queue
 
-		for b.empty() && !b.closed {
-			b.workCond.Wait()
-		}
-
-		if b.closed {
-			b.mu.Unlock()
+		if !ok {
 			return
 		}
 
-		ev, ok := b.next()
-		closing := b.closing
-
-		// Unlock early, so that we don't block any publisher in vain - but only if not closing
-		if !closing || !ok {
-			b.mu.Unlock()
-		}
-
-		if !ok {
-			continue
-		}
-
+		b.mu.RLock()
 		for _, sub := range b.subs[ev.sub] {
 			sub(ev.msg)
 		}
-
-		if closing {
-			b.mu.Unlock()
-		}
-
-		// Signal to all waiting publishers that there is capacity left
-		b.pubCond.Broadcast()
+		b.mu.RUnlock()
 	}
-}
-
-func (b *Bus) next() (ev event, ok bool) {
-	if b.empty() {
-		return
-	}
-
-	i := b.start
-	b.start = b.wrap(b.start + 1)
-	b.size--
-
-	return b.queue[i], true
 }
 
 func (b *Bus) _Pub(topic Topic, msg any) bool {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	for b.full() && !b.closed {
-		b.pubCond.Wait()
-	}
-
-	if b.closed {
-		return false
-	}
-
-	// Put value last in queue
-	i := b.idx(b.size)
-	b.size++
-
 	in := toIface(msg)
 
-	b.queue[i] = event{
+	b.queue <- event{
 		msg: in.data,
 		sub: subscription{
 			topic: topic,
@@ -213,17 +121,5 @@ func (b *Bus) _Pub(topic Topic, msg any) bool {
 		},
 	}
 
-	// Signal to one (1) waiting worker that there are events in queue
-	b.workCond.Signal()
 	return true
-}
-
-//go:inline
-func (b *Bus) idx(i int) int {
-	return b.wrap(b.start + i)
-}
-
-//go:inline
-func (b *Bus) wrap(i int) int {
-	return (i + b.maxSize) % b.maxSize
 }
--- bus_test.go
+++ bus_test.go
@@ -1,8 +1,11 @@
 package bus
 
-import "testing"
+import (
+	"runtime"
+	"testing"
+)
 
-func BenchmarkBusPub(b *testing.B) {
+func BenchmarkBusPubWithoutWorker(b *testing.B) {
 	type Foobar struct {
 		Id int
 	}
@@ -15,6 +18,64 @@ func BenchmarkBusPub(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Pub(eb, 0, v)
 	}
+
+	b.StopTimer()
+	eb.Close()
+}
+
+func BenchmarkBusPub(b *testing.B) {
+	type Foobar struct {
+		Id int
+	}
+
+	eb := NewBus(32)
+	v := &Foobar{Id: 1}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Pub(eb, 0, v)
+	}
+
+	b.StopTimer()
+	eb.Close()
+}
+
+func BenchmarkBusPubParallell(b *testing.B) {
+	type Foobar struct {
+		Id int
+	}
+
+	eb := NewBus(32)
+	v := &Foobar{Id: 1}
+
+	b.ResetTimer()
+
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			Pub(eb, 0, v)
+		}
+	})
+
+	b.StopTimer()
+	eb.Close()
+}
+
+func BenchmarkBusWithMultipleWorkers(b *testing.B) {
+	type Foobar struct {
+		Id int
+	}
+
+	eb := NewBusWithWorkers(32, runtime.GOMAXPROCS(-1))
+	v := &Foobar{Id: 1}
+
+	b.ResetTimer()
+
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			Pub(eb, 0, v)
+		}
+	})
 
 	b.StopTimer()
 	eb.Close()
